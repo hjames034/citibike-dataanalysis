87d466160f637ecb0a2c25dcf11c928c57468698
hjames034 <60247356+hjames034@users.noreply.github.com>
2022-12-08 17:53:35 -0800

    Create data_puller.py

---
+++ data_puller.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+from datetime import datetime
+
+# set the URL for the station status JSON feed
+url = 'https://gbfs.bikeshare.com/gbfs/en/station_status.json'
+
+# make a request to the URL and get the JSON response
+response = requests.get(url).json()
+
+# get the current date and time
+now = datetime.utcnow()
+
+# round the time to the nearest quarter hour
+minute = now.minute // 15 * 15
+now = now.replace(minute=minute)
+
+# add a column for the current date and time
+response['data']['stations']['last_update'] = now
+
+# convert the JSON response to a DataFrame
+df = pd.DataFrame(response['data']['stations'])
+
+# create a filename with the current date
+filename = 'station_data_' + now.strftime('%Y-%m-%d') + '.csv'
+
+# check if the file exists
+if os.path.exists(filename):
+    # if the file exists, append the data to it
+    df.to_csv(filename, mode='a', header=False)
+else:
+    # if the file doesn't exist, create a new file and write the data to it
+    df.to_csv(filename, mode='w', header=True)
